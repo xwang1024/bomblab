82913e3cee4b97d714ce40d1c69d07509e53553d
XWang1024 <xwang1024@126.com>
2017-03-22 21:54:14 +0800

    feat(bootloader): Mongo 加载完成后加载其他部分

--- index.js
+++ index.js
@@ -16,79 +16,78 @@ const cookieParser = require('cookie-parser');
 const bodyParser   = require('body-parser');
                      require('body-parser-xml')(bodyParser);
 
-const route = require('./lib/route')();
-const initPassport = require('./lib/passport');
 const config = require('./lib/config');
-const Wechat = require('./lib/service/Wechat');
-const Mongo  = require('./lib/service/Mongo');
-const Cache  = require('./lib/service/Cache');
-const Xiumi = require('./lib/service/Xiumi');
 
 const app = express();
 app.config = config;
 
-// init mongodb connection
+const Mongo  = require('./lib/service/Mongo');
 Mongo.getClient(config.mongodb).then((mongoClient) => {
   app.db = mongoClient;
   require('./lib/model')(app, mongoose);
-});
 
-Cache.init(config.redis);
-Wechat.init(config.wechat);
-Xiumi.init(config.xiumi);
-qiniu.conf.ACCESS_KEY = config.qiniu.ACCESS_KEY;
-qiniu.conf.SECRET_KEY = config.qiniu.SECRET_KEY;
+  const route = require('./lib/route')();
+  const initPassport = require('./lib/passport');
+  const Wechat = require('./lib/service/Wechat');
+  const Cache  = require('./lib/service/Cache');
+  const Xiumi = require('./lib/service/Xiumi');
 
-// view engine setup
-var hbs = exphbs.create({
-  extname: ".hbs",
-  defaultLayout: 'default',
-  helpers: require('./lib/hbs_helpers')
-});
-app.engine('hbs', hbs.engine);
-app.set('view engine', 'hbs');
+  Cache.init(config.redis);
+  Wechat.init(config.wechat);
+  Xiumi.init(config.xiumi);
+  qiniu.conf.ACCESS_KEY = config.qiniu.ACCESS_KEY;
+  qiniu.conf.SECRET_KEY = config.qiniu.SECRET_KEY;
 
-morgan.token('username', (req) => (req.user ? req.user.username : 'unknown') );
-app.use(morgan(':remote-addr :username :method :url HTTP/:http-version :status :res[content-length] - :response-time ms', {
-  skip: (req, res) => { return /(.*\.css$)|(.*\.js$)|(.*\.ico$)|(.*\.png$)|(.*\.woff$)|(.*\.woff2$)/.test(req.path); }
-}));
-
-app.use(bodyParser.json({limit: '3mb'}));
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.xml());
-app.use(cookieParser());
-app.use(session({
-  resave: true,
-  saveUninitialized: true,
-  secret: config.cryptoKey,
-  store: new mongoStore({ url: config.mongodb.uri })
-}));
-app.use(passport.initialize());
-app.use(passport.session());
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(busboy());
-
-initPassport(app, passport);
-
-app.use('/', route);
-
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  res.status(404);
-  res.render('error/404', {
-    layout: 'single',
-    message: err.message,
-    error: {}
+  var hbs = exphbs.create({
+    extname: ".hbs",
+    defaultLayout: 'default',
+    helpers: require('./lib/hbs_helpers')
   });
-});
+  app.engine('hbs', hbs.engine);
+  app.set('view engine', 'hbs');
 
-app.use(function(err, req, res, next) {
-  console.error(err.stack);
-  res.status(err.status || 500);
-  res.render('error/500', {
-    layout: 'single',
-    message: err.message,
-    stackInfo: err.stack
+  morgan.token('username', (req) => (req.user ? req.user.username : 'unknown') );
+  app.use(morgan(':remote-addr :username :method :url HTTP/:http-version :status :res[content-length] - :response-time ms', {
+    skip: (req, res) => { return /(.*\.css$)|(.*\.js$)|(.*\.ico$)|(.*\.png$)|(.*\.woff$)|(.*\.woff2$)/.test(req.path); }
+  }));
+
+  app.use(bodyParser.json({limit: '3mb'}));
+  app.use(bodyParser.urlencoded({ extended: false }));
+  app.use(bodyParser.xml());
+  app.use(cookieParser());
+  app.use(session({
+    resave: true,
+    saveUninitialized: true,
+    secret: config.cryptoKey,
+    store: new mongoStore({ url: config.mongodb.uri })
+  }));
+  app.use(passport.initialize());
+  app.use(passport.session());
+  app.use(express.static(path.join(__dirname, 'public')));
+  app.use(busboy());
+
+  initPassport(app, passport);
+
+  app.use('/', route);
+
+  app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    res.status(404);
+    res.render('error/404', {
+      layout: 'single',
+      message: err.message,
+      error: {}
+    });
+  });
+
+  app.use(function(err, req, res, next) {
+    console.error(err.stack);
+    res.status(err.status || 500);
+    res.render('error/500', {
+      layout: 'single',
+      message: err.message,
+      stackInfo: err.stack
+    });
   });
 });
 
